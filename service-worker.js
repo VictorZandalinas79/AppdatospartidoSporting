407c612134102381361ed8aaa73b467c0329e859
VictorZandalinas79 <145045831+VictorZandalinas79@users.noreply.github.com>
2024-06-06 18:49:32 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,60 +1,47 @@
-// service-worker.js
+// This is the "Offline page" service worker
 
-const CACHE_NAME = 'mi-cache-v1';
-const urlsToCache = [
-  '/registrominutaje1%20copia.html',
-  '/cronometroWorker.js',
-  '/fondo.png',
-  '/iconofutbol.png',
-  '/jugadoressporting.html',
-  '/jugadoressporting.json',
-  '/jugadoressporting1.html',
-  '/jugadoressporting1.json',
-  '/jugadoressporting2.json',
-  '/jugadoressporting22.html',
-  '/manifest.json',
-  // Añade aquí otras URLs que necesites cachear
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Instalación del Service Worker
-self.addEventListener('install', event => {
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Archivos cacheados');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-// Manejo de solicitudes de red
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Devuelve el recurso de la caché si está disponible
-        if (response) {
-          return response;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-        // Realiza una solicitud de red en caso contrario
-        return fetch(event.request);
-      }
-    )
-  );
-});
 
-// Activación del Service Worker y limpieza de cachés antiguos
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
