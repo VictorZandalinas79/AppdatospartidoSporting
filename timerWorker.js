1230cce7c9d741e5aba3505675282ec170bbaeff
VictorZandalinas79 <145045831+VictorZandalinas79@users.noreply.github.com>
2024-07-11 18:53:12 +0200

    Update timerWorker.js

--- timerWorker.js
+++ timerWorker.js
@@ -1,17 +1,33 @@
-let count = 0;
-let timerId = null;
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
 
-onmessage = function(e) {
-    if (e.data === 'start') {
-        timerId = setInterval(() => {
-            count += 10;
-            postMessage(count);
-        }, 10);
-    } else if (e.data === 'stop') {
-        clearInterval(timerId);
-        timerId = null;
-    } else if (e.data === 'reset') {
-        count = 0;
-        postMessage(count);
-    }
+self.onmessage = function(e) {
+  if (e.data.command === 'start') {
+    startTimer();
+  } else if (e.data.command === 'stop') {
+    stopTimer();
+  } else if (e.data.command === 'reset') {
+    resetTimer();
+  }
 };
+
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(updateTimer, 10);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function resetTimer() {
+  stopTimer();
+  elapsedTime = 0;
+  self.postMessage({time: elapsedTime});
+}
+
+function updateTimer() {
+  elapsedTime = Date.now() - startTime;
+  self.postMessage({time: elapsedTime});
+}
